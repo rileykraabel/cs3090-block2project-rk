05e2baca7cd1b1e356c5e99074027cdcdf49b8f0
RILEY KRAABEL <u1354812@umail.utah.edu>
2023-02-23 15:09:52 -0700

    Add project files.

---
+++ block2project/UsefulCode.cs
@@ -0,0 +1,17 @@
+﻿/* This is a class that can be distributed and added to by contributors who decide to.
+ * This class is protected by a MIT License. 
+ * Author: Riley Kraabel
+ */
+namespace block2project
+{
+    internal class UsefulCode
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Riley's Code is so useful and important " + i);
+            }
+        }
+    }
+}
