338fa4d2c1f2bc5b0e1bdfc19693164209ddcbf0
RILEY KRAABEL <u1354812@umail.utah.edu>
2023-02-23 15:11:38 -0700

    updated the super useful code.

--- block2project/UsefulCode.cs
+++ block2project/UsefulCode.cs
@@ -4,13 +4,21 @@
  */
 namespace block2project
 {
+    /// <summary>
+    ///     This class contains many useful methods for the users and developers.
+    /// </summary>
     internal class UsefulCode
     {
         static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Riley's Code is so useful and important " + i);
+                Console.WriteLine("Riley's code is so useful and important " + i);
+            }
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("This counts numbers 10-1. Important number: " + i);
             }
         }
     }
